c11fe8adf707ea445b0cda9c40fcc9f4c637709d
ankit3jan <152656573+ankit3jan@users.noreply.github.com>
2025-04-23 13:14:28 +0530

    rechecked

--- service-worker.js
+++ service-worker.js
@@ -4,11 +4,11 @@ const urlsToCache = [
     "/index.html",
     "/style.css",
     "/app.js",
-    "normalize.css",
-    "destination.html",
-    "gallery.html",
-    "contact.html",
-    "about.html",
+    "/normalize.css",
+    "/destination.html",
+    "/gallery.html",
+    "/contact.html",
+    "/about.html",
     "/assets/beach.jpeg",
     "/assets/city.jpeg",
     "/assets/forest.jpeg",
@@ -19,13 +19,13 @@ const urlsToCache = [
     "/assets/mountain.jpg",
     "/assets/paris.jpg",
     "/assets/pexels-miki-czetti-26377-111963.jpg",
-    "/assets\pexels-miki-czetti-26377-111963.webp",
+    "/assets/pexels-miki-czetti-26377-111963.webp",
     "/assets/pexels-pixabay-237272.jpg",
     "/assets/pexels-pixabay-237272.webp",
     "/assets/pexels-souvenirpixels-1531660.jpg",
     "/assets/pexels-souvenirpixels-1531660.webp",
     "/assets/tokyo.jpg",
-    "/assets/Travel-Wallpaper-Free-Download.jpg",
+    "/assets/Travel-Wallpaper-Free-Download.jpg"
     
 ];
 
